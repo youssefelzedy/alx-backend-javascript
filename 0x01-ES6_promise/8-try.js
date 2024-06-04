46dfca423b5e9f5d300eb59b331368b24a3fc669
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-04 22:39:21 +0300

    done 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,7 @@
 export default function divideFunction(numerator, denominator) {
   return new Promise((resolve, reject) => {
     if (denominator === 0) {
-      reject(new Error('cannot divide by 0'));
+      reject(Error('cannot divide by 0'));
     } else {
       resolve(numerator / denominator);
     }
