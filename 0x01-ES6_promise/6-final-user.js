a2544b7b0750f2ade6b276bbec2cc879d781732a
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-04 22:23:17 +0300

    done 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default async function handleProfileSignup(firstName, lastName, filename) {
   return Promise
     .allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)])
     .then((values) => (
