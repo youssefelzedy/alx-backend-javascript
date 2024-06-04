5796915e43b631178f5dbffdfd9d6d337f007bd4
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-04 20:49:53 +0300

    done 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,6 @@ export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((values) => {
       console.log(values[0].body, values[1].firstName, values[1].lastName);
-    });
+    })
+    .catch(() => console.log('Signup system offline'));
 }
