b232e173f7f2fec5a5df6a187662ed7e0c76035e
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-16 03:31:29 +0300

    finsh task

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import ClassRoom from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof ClassRoom)) {
+      throw new TypeError('Currency must be a Currency');
+    }
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+}
