c7e71d3c358eba91b67160c6aa351489bf457e0b
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-16 03:25:15 +0300

    finsh task

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,40 +1,40 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
-    return this._name;
+    return this.name;
   }
 
   get length() {
-    return this._length;
+    return this.length;
   }
 
   get students() {
-    return this._students;
+    return this.students;
   }
 
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = newName;
+    this.name = newName;
   }
 
   set length(newLength) {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._length = newLength;
+    this.length = newLength;
   }
 
   set students(newStudents) {
     if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    this._students = newStudents;
+    this.students = newStudents;
   }
 }
