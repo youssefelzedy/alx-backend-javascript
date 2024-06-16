aa7b93e2ef22bde03a832fcecbbf33b1e3d80ef4
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-16 19:51:44 +0300

    finsh task

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,31 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set size(newSize) {
+    this._size = newSize;
+  }
+
+  set location(newLocation) {
+    this.__location = newLocation;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    } if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
