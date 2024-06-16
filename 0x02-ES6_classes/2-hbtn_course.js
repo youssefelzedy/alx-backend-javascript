227fae8cd240d669712c3423fd42ff7dbd99743b
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-16 03:25:47 +0300

    finsh task

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -6,35 +6,35 @@ export default class HolbertonCourse {
   }
 
   get name() {
-    return this.name;
+    return this._name;
   }
 
   get length() {
-    return this.length;
+    return this._length;
   }
 
   get students() {
-    return this.students;
+    return this._students;
   }
 
   set name(newName) {
     if (typeof newName !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this.name = newName;
+    this._name = newName;
   }
 
   set length(newLength) {
     if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this.length = newLength;
+    this._length = newLength;
   }
 
   set students(newStudents) {
     if (!Array.isArray(newStudents) || !newStudents.every((student) => typeof student === 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
-    this.students = newStudents;
+    this._students = newStudents;
   }
 }
