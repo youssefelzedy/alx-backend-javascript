55942a7fd0f313e4547f63b16ec92a386458a5a8
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-17 08:29:24 +0300

    finsh task

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,39 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set brand(newBrand) {
+    this._brand = newBrand;
+  }
+
+  set motor(newMotor) {
+    this._motor = newMotor;
+  }
+
+  set color(newColor) {
+    this._color = newColor;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    return new this.constructor[Symbol.species](Symbol.species);
+  }
+}
