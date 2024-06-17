9d0d1c93abef25fde8c6c74a48934645be84bdb1
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-17 08:40:09 +0300

    finsh task

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -15,6 +15,8 @@ export default class EVCar extends Car {
   }
 
   cloneCar() {
-    return new super.constructor[Symbol.species]();
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
   }
 }
