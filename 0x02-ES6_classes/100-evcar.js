ace3720d5d28b288723b833a68aef2f2734d9a82
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-17 08:36:40 +0300

    finsh task

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,20 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(newRange) {
+    this._range = newRange;
+  }
+
+  cloneCar() {
+    return new super.constructor[Symbol.species]();
+  }
+}
